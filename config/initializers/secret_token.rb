ca0cd73e50d059ae6d97e5ff0254391210d49ea2
exactreal <exactreal@gmail.com>
2010-10-24 23:39:42 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saveup::Application.config.secret_token = 'daac4d404060986b43cede628931eacde5a8d7cfcb73eda700f85b4b5af6fc5e2bae48e621339bd46ca95dea4bcafea90a0bcdf4e3174302464c13851391742d'
---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+# START:routes
+Saveup::Application.routes.draw do |map|
+  
+  resources :businesses
+
+  resources :goals do
+    resources :credits
+  end
+# END:routes
+
+  root :to => "goals#index"
+
+  map.resources :sessions, :users
+
+  match 'login'  => 'sessions#new',     :as => :login
+  match 'logout' => 'sessions#destroy', :as => :logout
+  match 'join'   => 'users#new',        :as => :join
+  match 'about'  => 'info#index',       :as => :about
+
+# START:routes
+
+end
+# END:routes
